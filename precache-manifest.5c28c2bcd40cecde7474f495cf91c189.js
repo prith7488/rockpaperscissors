bdb4dcb1ade258ca8a85cc0673e316682181ebba
prith7488 <prith7488@gmail.com>
2020-04-19 21:55:22 +0530

    Updates

---
+++ precache-manifest.5c28c2bcd40cecde7474f495cf91c189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02a1f24b566d1fa9c6561bbeee98a8f",
+    "url": "/rockpaperscissors/index.html"
+  },
+  {
+    "revision": "b88df91321506b66136b",
+    "url": "/rockpaperscissors/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "043ce22b1e6e38c67264",
+    "url": "/rockpaperscissors/static/js/2.82c394a7.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/rockpaperscissors/static/js/2.82c394a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88df91321506b66136b",
+    "url": "/rockpaperscissors/static/js/main.83348b1e.chunk.js"
+  },
+  {
+    "revision": "b6f4658ce6702b05ef1f",
+    "url": "/rockpaperscissors/static/js/runtime-main.8621cb9c.js"
+  }
+]);
